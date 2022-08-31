b180e5c014f7c3b961ce17b104b60f01bcef9072
Clari Ursini <clari_ursini@hotmail.com>
2022-08-30 23:00:12 -0300

    Updated veterinary, vaccination and studies controllers

--- app/controllers/vaccinations_controller.rb
+++ app/controllers/vaccinations_controller.rb
@@ -1,2 +1,37 @@
 class VaccinationsController < ApplicationController
+  def index
+    @pet = Pet.find(params[:id]).clinical_histories.id
+    @vaccinations = Vaccination.where(clinical_history_id: @pet)
+  end
+
+  def new
+    @vaccination = Vaccination.new
+  end
+
+  def create
+    @vaccination = Vaccination.new(vaccination_params)
+    @vaccination.clinical_history_id = Pet.find(params[:id]).clinical_histories
+    if @vaccination.save
+      redirect_to vaccinations_path
+    else
+      render 'vaccinations/new', status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @vaccination = Vaccination.find(params[:id])
+  end
+
+  def update
+    @vaccination = Vaccination.find(params[:id])
+    @vaccination.update(vaccination_params)
+    redirect_to vaccinations_path
+  end
+
+  private
+
+  def vaccination_params
+    params.require(:vaccination).permit(:expiration_date, :photo, :vaccine_id)
+  end
+
 end
--- app/controllers/veterinaries_controller.rb
+++ app/controllers/veterinaries_controller.rb
@@ -1,2 +1,18 @@
 class VeterinariesController < ApplicationController
+  def index
+    if params[:query].present? # si la query esta presente
+      if User.search_by_location(params[:query]).size.positive? # Si la query encuentra algo
+        @veterinaries = User.where(type_of_user: "Veterinary")
+        @veterinaries.search_by_location(params[:query])
+      else
+        "No results found" # Si no encuentra resultados
+      end
+    else
+      @veterinaries = User.where(type_of_user: "Veterinary") # Si no buscamos nada que traiga todas
+    end
+  end
+
+  def show
+    @veterinary = User.find(params[:id])
+  end
 end
