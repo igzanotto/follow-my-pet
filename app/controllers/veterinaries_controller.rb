211426ab8fc4673b4439a7914de1034efa2ca67e
Carolina Arlia <carolinaiarlia@gmail.com>
2022-09-01 21:38:18 +0200

    wip

--- app/controllers/veterinaries_controller.rb
+++ app/controllers/veterinaries_controller.rb
@@ -1,5 +1,6 @@
 class VeterinariesController < ApplicationController
   def index
+    @pet = Pet.find(params[:pet_id])
     if params[:query].present? # si la query esta presente
       if User.search_by_location(params[:query]).size.positive? # Si la query encuentra algo
         @veterinaries = User.where(type_of_user: "Veterinary")
