ba3929332e84cddc666f3fa3da11ad8fa56f56d1
sofia larrea <sofialejandra.larrea@gmail.com>
2022-09-05 01:49:12 -0300

    Navabar, falta una parte

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -10,8 +10,14 @@ application.register("hello", HelloController)
 import MapController from "./map_controller"
 application.register("map", MapController)
 
+import NavbarController from "./navbar_controller"
+application.register("navbar", NavbarController)
+
 import SpecialityController from "./speciality_controller"
 application.register("speciality", SpecialityController)
 
 import TypedJsController from "./typed_js_controller"
 application.register("typed-js", TypedJsController)
+
+import NavbarController from "./navbar_controller.js"
+application.register("navbar", NavbarController)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_01_202614) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_04_050118) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -63,6 +63,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_01_202614) do
     t.string "type_of_history"
     t.index ["pet_id"], name: "index_clinical_histories_on_pet_id"
     t.index ["user_id"], name: "index_clinical_histories_on_user_id"
+  end
+
+  create_table "consultations", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.datetime "star_time"
+    t.datetime "end_time"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "pets", force: :cascade do |t|
