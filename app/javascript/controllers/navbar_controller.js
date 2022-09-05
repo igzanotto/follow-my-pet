0bf03bb8c943865aa2f323595758b7c53b4d834c
sofia larrea <sofialejandra.larrea@gmail.com>
2022-09-05 10:29:58 -0300

    Sofi navbar

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -3,7 +3,7 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="navbar"
 export default class extends Controller {
   connect() {
-  console.log("heloo from controller navbar")
+  
 }
 
   updateNavbar() {
