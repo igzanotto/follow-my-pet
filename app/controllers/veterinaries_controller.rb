21420cb148e3bb6c2d9b640360359f2284c5e273
Clari Ursini <clari_ursini@hotmail.com>
2022-09-06 18:35:42 -0300

    updated views

--- app/controllers/veterinaries_controller.rb
+++ app/controllers/veterinaries_controller.rb
@@ -38,6 +38,7 @@ class VeterinariesController < ApplicationController
     @pet = Pet.find(params[:pet_id])
     @veterinary = User.find(params[:id])
     @appointment = Appointment.new
+
     @review = Review.new
     @reviews = Review.where(user: @veterinary).last(3)
     @reviews_filtered = Review.where(user: @veterinary).average(:rating)
